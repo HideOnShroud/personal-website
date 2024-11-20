c6071dd0937cb3dd2a9bb11f7e523af88ef3527f
HideOnShroud <natroshviliguga5@gmail.com>
2024-11-20 19:50:02 +0400

    added quantori cert

--- src/data/certList.ts
+++ src/data/certList.ts
@@ -3,6 +3,7 @@ import itml from '../assets/certificates/intro-to-ml.png'
 import iml from '../assets/certificates/intermediate-ml.png'
 import feature from '../assets/certificates/feature-eng.png'
 import itdl from '../assets/certificates/intro-to-dl.png'
+import psq from '../assets/certificates/Quantori-Python.png'
 
 const certList = [
     { title: 'Exadel Mentorship', image: exadel, link: 'https://certificates.exadel.com/399143c3-a20f-4013-aa2c-c9c9784eb1c6#acc.kOvozxU8' },
@@ -10,6 +11,7 @@ const certList = [
     { title: 'Kaggle Intermediate Machine Learning', image: iml, link: 'https://www.kaggle.com/learn/certification/guganatroshvili/intermediate-machine-learning' },
     { title: 'Kaggle Feature Engineering', image: feature, link: 'https://www.kaggle.com/learn/certification/guganatroshvili/feature-engineering' },
     { title: 'Kaggle Intro to Deep Learning', image: itdl, link: 'https://www.kaggle.com/learn/certification/guganatroshvili/intro-to-deep-learning' },
+    { title: 'Quantori Academy Python School', image: psq, link: 'https://quantori.com/academy' }
 
 ]
 
