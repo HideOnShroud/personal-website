c0f77b1b60d085b571b3640fc95b56c114c0983b
HideOnShroud <natroshviliguga5@gmail.com>
2025-02-27 00:34:02 +0400

    SOC Cert

--- src/data/certList.ts
+++ src/data/certList.ts
@@ -4,8 +4,10 @@ import iml from '../assets/certificates/intermediate-ml.png'
 import feature from '../assets/certificates/feature-eng.png'
 import itdl from '../assets/certificates/intro-to-dl.png'
 import psq from '../assets/certificates/Quantori-Python.png'
+import tbcSOC from '../assets/certificates/TBCSOC.png'
 
 const certList = [
+    { title: 'TBC & SAID Advanced Cybersecurity Course', image: tbcSOC, link: 'https://www.tbcacademy.ge/usaid/information-security-advance' },
     { title: 'Exadel Mentorship', image: exadel, link: 'https://certificates.exadel.com/399143c3-a20f-4013-aa2c-c9c9784eb1c6#acc.kOvozxU8' },
     { title: 'Kaggle Intro to Machine Learning', image: itml, link: 'https://www.kaggle.com/learn/certification/guganatroshvili/intro-to-machine-learning' },
     { title: 'Kaggle Intermediate Machine Learning', image: iml, link: 'https://www.kaggle.com/learn/certification/guganatroshvili/intermediate-machine-learning' },
